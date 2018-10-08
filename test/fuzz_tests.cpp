37cee7e34213ce19ce805828e335612d17cf1166
Shubham Lagwankar <shubhu105@gmail.com>
2018-10-08 18:04:47 -0400

    Use Catch for testing

---
+++ test/fuzz_tests.cpp
@@ -0,0 +1,91 @@
+#include "radix_tree.hpp"
+
+#include <catch.hpp>
+
+#include <cstdlib>
+#include <ctime>
+#include <random>
+#include <string>
+#include <unordered_map>
+
+namespace
+{
+
+bool tree_insert(radix_tree& tree, std::string const& key)
+{
+    auto* data = reinterpret_cast<const unsigned char*>(key.data());
+    return tree.insert(data, key.size());
+}
+
+bool tree_erase(radix_tree& tree, std::string const& key)
+{
+    auto* data = reinterpret_cast<const unsigned char*>(key.data());
+    return tree.erase(data, key.size());
+}
+
+std::string random_key(std::minstd_rand& rng, std::size_t key_length)
+{
+    const char* chars = "abcdefghijklmnopqrstuvwxyz0123456789";
+    const int size = 36;
+
+    std::string key;
+    key.reserve(key_length);
+
+    for (std::size_t i = 0; i < key_length; ++i)
+        key.push_back(chars[rng() % size]);
+
+    return key;
+}
+
+constexpr std::size_t operations = 100000;
+
+}
+
+
+TEST_CASE("fuzz", "[fuzz]")
+{
+    radix_tree tree;
+    std::unordered_map<std::string, std::size_t> set;
+    std::size_t set_size = 0;
+    std::size_t key_length = 50;
+
+    std::minstd_rand rng;
+    rng.seed(static_cast<unsigned>(std::time(nullptr)));
+
+    for (std::size_t i = 0; i < operations; ++i) {
+        std::string key;
+        if (rng() % 2) {
+            std::size_t len = (static_cast<std::size_t>(rng())
+                               % key_length) + 1;
+            key = random_key(rng, len);
+            INFO("insert: " << key);
+            bool tree_result = tree_insert(tree, key);
+            bool set_result = ++set[key] == 1;
+            ++set_size;
+            REQUIRE(tree_result == set_result);
+        } else if (set_size > 0) {
+            auto idx = (static_cast<std::size_t>(rng()) % set.size());
+            for (auto& item : set) {
+                if (idx == 0) {
+                    key = item.first;
+                    break;
+                }
+                --idx;
+            }
+            INFO("erase: " << key);
+            bool set_result =
+                [&set,&key,&set_size]() {
+                if (set[key] == 0)
+                    return false;
+                --set[key];
+                --set_size;
+                return true;
+            }();
+            bool tree_result = !key.empty() && tree_erase(tree, key);
+            REQUIRE(tree_result == set_result);
+        }
+        REQUIRE(set_size == tree.size());
+    }
+
+    REQUIRE(set_size == tree.size());
+}
---
+++ test/unit_tests.cpp
@@ -0,0 +1,186 @@
+#include "radix_tree.hpp"
+
+#include <catch.hpp>
+
+#include <cstdlib>
+#include <string>
+#include <vector>
+
+namespace
+{
+
+bool tree_insert(radix_tree& tree, std::string const& key)
+{
+    auto* data = reinterpret_cast<const unsigned char*>(key.data());
+    return tree.insert(data, key.size());
+}
+
+bool tree_erase(radix_tree& tree, std::string const& key)
+{
+    auto* data = reinterpret_cast<const unsigned char*>(key.data());
+    return tree.erase(data, key.size());
+}
+
+bool tree_contains(radix_tree const& tree, std::string const& key)
+{
+    auto* data = reinterpret_cast<const unsigned char*>(key.data());
+    return tree.contains(data, key.size());
+}
+
+}
+
+
+TEST_CASE("insertion", "[insert]")
+{
+    radix_tree tree;
+
+    REQUIRE(tree.size() == 0);
+
+    SECTION("single key")
+    {
+        REQUIRE(tree_insert(tree, "key"));
+    }
+
+    SECTION("return whether inserted key was not already in the tree")
+    {
+        REQUIRE(tree_insert(tree, "test"));
+        REQUIRE_FALSE(tree_insert(tree, "test"));
+    }
+
+    SECTION("check if size is updated correctly")
+    {
+        // Adapted from the example in the wikipedia article.
+    }
+}
+
+TEST_CASE("deletion", "[erase]")
+{
+    radix_tree tree;
+
+    SECTION("empty tree")
+    {
+        REQUIRE_FALSE(tree_erase(tree, "waldo"));
+    }
+
+    SECTION("single key")
+    {
+        tree_insert(tree, "key");
+        REQUIRE(tree_erase(tree, "key"));
+    }
+
+    SECTION("common prefix of two keys")
+    {
+        tree_insert(tree, "checkpoint");
+        tree_insert(tree, "checklist");
+        REQUIRE_FALSE(tree_erase(tree, "check"));
+    }
+
+    SECTION("delete single key twice")
+    {
+        tree_insert(tree, "key");
+        REQUIRE(tree_erase(tree, "key"));
+        REQUIRE_FALSE(tree_erase(tree, "key"));
+    }
+}
+
+TEST_CASE("lookup", "[contains]")
+{
+    radix_tree tree;
+
+    SECTION("empty tree")
+    {
+        REQUIRE_FALSE(tree_contains(tree, "key"));
+    }
+
+    SECTION("inserted key")
+    {
+        tree_insert(tree, "key");
+        REQUIRE(tree_contains(tree, "key"));
+    }
+
+    SECTION("common prefix of two inserted keys")
+    {
+        tree_insert(tree, "introduce");
+        tree_insert(tree, "introspect");
+        REQUIRE_FALSE(tree_contains(tree, "intro"));
+    }
+
+    SECTION("prefix of an inserted key")
+    {
+        tree_insert(tree, "toasted");
+        REQUIRE_FALSE(tree_contains(tree, "toast"));
+        REQUIRE_FALSE(tree_contains(tree, "toaste"));
+    }
+
+    SECTION("key not in the tree")
+    {
+        tree_insert(tree, "red");
+        REQUIRE_FALSE(tree_contains(tree, "blue"));
+    }
+}
+
+TEST_CASE("check if size is updated correctly")
+{
+    radix_tree tree;
+
+    // Adapted from the example on wikipedia.
+    std::vector<std::string> keys = {
+        "tester", "water", "slow", "slower", "test", "team", "toast"
+    };
+
+    for (auto const& key : keys)
+        REQUIRE(tree_insert(tree, key));
+    REQUIRE(tree.size() == keys.size());
+    for (auto const& key : keys)
+        REQUIRE_FALSE(tree_insert(tree, key));
+    REQUIRE(tree.size() == 2 * keys.size());
+    for (auto const& key : keys)
+        REQUIRE(tree_erase(tree, key));
+    REQUIRE(tree.size() == keys.size());
+    for (auto const& key : keys)
+        REQUIRE(tree_erase(tree, key));
+    REQUIRE(tree.size() == 0);
+}
+
+TEST_CASE("tree structure", "[print]")
+{
+    radix_tree tree;
+
+    SECTION("delete node with a single outgoing edge")
+    {
+        tree_insert(tree, "test");
+        tree_insert(tree, "testing");
+        tree_erase(tree, "test");
+        tree.print();
+    }
+
+    SECTION("node with a parent that has two outgoing edges")
+    {
+        tree_insert(tree, "tester");
+        tree_insert(tree, "testing");
+        tree_erase(tree, "tester");
+        tree.print();
+    }
+
+    SECTION("insertion cases")
+    {
+        std::vector<std::string> keys = {
+            "test", "toaster", "toasting", "to"
+        };
+
+        for (auto const& key : keys)
+            tree_insert(tree, key);
+        tree.print();
+    }
+
+    SECTION("simple example")
+    {
+        std::vector<std::string> keys = {
+            "tester", "water", "slow", "slower", "test", "team", "toast"
+        };
+
+        for (auto const& key : keys)
+            tree_insert(tree, key);
+        tree.print();
+    }
+}
